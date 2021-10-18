79e5b7a2e94002a4dd5d71d2385963f3bf63a124
Tawsif05 <tawsifmahmud05@gmail.com>
2021-10-18 20:13:54 +0600

    day2 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_catalog/home_page.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({ Key? key }) : super(key: key);
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Material(
-        child: Center(
-          child: Container(child: Text("Flutter"),),
-        ),
-      ),
-    );
+    return HomePage();
   }
 }
