8384a5339517f76651b71fa0d7170069a7afdc91
Tawsif05 <tawsifmahmud05@gmail.com>
2021-10-28 13:03:30 +0600

    completed day12

--- lib/models/catalog.dart
+++ lib/models/catalog.dart
@@ -1,3 +1,15 @@
+class CatalogModel {
+  static final items = [
+    Item(
+        "Codepur001",
+        "iPhone 12 Pro",
+        "Apple iPhone 12th generation",
+        999,
+        "#33505a",
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRISJ6msIu4AU9_M9ZnJVQVFmfuhfyJjEtbUm3ZK11_8IV9TV25-1uM5wHjiFNwKy99w0mR5Hk&usqp=CAc")
+  ];
+}
+
 class Item {
   final String id;
   final String name;
@@ -8,13 +20,3 @@ class Item {
 
   Item(this.id, this.name, this.desc, this.price, this.color, this.image);
 }
-
-final products = [
-  Item(
-      "Codepur001",
-      "iPhone 12 Pro",
-      "Apple iPhone 12th generation",
-      999,
-      "#33505a",
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRISJ6msIu4AU9_M9ZnJVQVFmfuhfyJjEtbUm3ZK11_8IV9TV25-1uM5wHjiFNwKy99w0mR5Hk&usqp=CAc")
-];
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_catalog/models/catalog.dart';
 import 'package:flutter_catalog/widgets/drawer.dart';
+import 'package:flutter_catalog/widgets/item_widget.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final dummyList = List.generate(20, (index) => CatalogModel.items[0]);
     return Scaffold(
       appBar: AppBar(
         title: Text(
@@ -13,11 +16,13 @@ class HomePage extends StatelessWidget {
           style: TextStyle(color: Colors.black),
         ),
       ),
-      body: Center(
-        child: Container(
-          child: Text("Flutter"),
-        ),
-      ),
+      body: ListView.builder(
+          itemCount: dummyList.length,
+          itemBuilder: (context, index) {
+            return ItemWidget(
+              item: dummyList[index],
+            );
+          }),
       drawer: MyDrawer(),
     );
   }
